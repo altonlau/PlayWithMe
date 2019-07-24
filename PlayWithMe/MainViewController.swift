687b8c4244abd7184bd9fb8306576183b11fe211
Alton Lau <altonlau@yelp.com>
2019-07-24 11:20:55 -0700

    Creating base project for development.

---
+++ PlayWithMe/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  PlayWithMe
+//
+//  Created by Alton Lau on 7/24/19.
+//  Copyright © 2019 Yelp Inc. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+}
