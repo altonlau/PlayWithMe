c9f313ce34371397fb89cbd787367133eb357cac
Stephen Lee <stephel@yelp.com>
2019-07-24 16:39:45 -0700

    WIP on settings view controller

--- PlayWithMe/Models/Chat.swift
+++ PlayWithMe/Models/Chat.swift
@@ -1,23 +1,26 @@
 //
-//  Group.swift
+//  User.swift
 //  PlayWithMe
 //
 //  Created by Stephen Lee on 7/24/19.
 //  Copyright © 2019 Yelp Inc. All rights reserved.
 //
-
 import Foundation
 import CoreLocation
 
 struct Chat: CustomStringConvertible {
-    var description: String { return "\(id) \(name) \(location)"}
+    var description: String { return "\(id) \(name) \(activityType) \(location)"}
 
     var id: String
-    var messages: [Message]
-    var name: String
+    var activityType: String
+    var imageURL: String
     var latitude: CLLocationDegrees
     var longitude: CLLocationDegrees
     var location: CLLocation {
         return CLLocation(latitude: latitude, longitude: longitude)
     }
+    var name: String
+    var chatIds: [String]
+    
+    
 }
--- PlayWithMe/Models/User.swift
+++ PlayWithMe/Models/User.swift
@@ -1,26 +1,28 @@
 //
-//  User.swift
+//  Group.swift
 //  PlayWithMe
 //
 //  Created by Stephen Lee on 7/24/19.
 //  Copyright © 2019 Yelp Inc. All rights reserved.
 //
+
 import Foundation
 import CoreLocation
 
 struct User: CustomStringConvertible {
-    var description: String { return "\(id) \(name) \(activityType) \(location)"}
+    var description: String { return "\(id) \(name) \(location)"}
 
     var id: String
-    var activityType: String
-    var imageURL: String
-    var latitude: CLLocationDegrees
+    var chatIds: [String]
+    var firstName: String
+    var lastName: String
+    var name: String {
+        return "\(firstName) \(lastName)"
+    }
     var longitude: CLLocationDegrees
+    var latitude: CLLocationDegrees
     var location: CLLocation {
         return CLLocation(latitude: latitude, longitude: longitude)
     }
-    var name: String
-    var chatIds: [String]
-    
     
 }
