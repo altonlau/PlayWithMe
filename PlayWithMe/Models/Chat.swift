b6bf23e17230e3864f70c78b16c82c1e5e7a44b1
Stephen Lee <stephel@yelp.com>
2019-07-24 14:59:40 -0700

    Add models

--- PlayWithMe/Models/Chat.swift
+++ PlayWithMe/Models/Chat.swift
@@ -1,17 +1,20 @@
 //
-//  User.swift
+//  Group.swift
 //  PlayWithMe
 //
 //  Created by Stephen Lee on 7/24/19.
 //  Copyright © 2019 Yelp Inc. All rights reserved.
 //
+
 import Foundation
 import CoreLocation
 
-struct User: CustomStringConvertible {
-    var description: String { return "\(firstName) \(lastName)"}
-    var firstName: String
-    var lastName: String
+struct Chat: CustomStringConvertible {
+    var description: String { return "\(id) \(name) \(location)"}
+
+    var id: String
+    var messages: [Message]
+    var name: String
     var latitude: CLLocationDegrees
     var longitude: CLLocationDegrees
     var location: CLLocation {
