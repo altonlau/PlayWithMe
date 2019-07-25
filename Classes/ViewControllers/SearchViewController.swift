c724ae2fe1c075183ea53a6d333c5d752a74e21a
Alton Lau <altonlau@yelp.com>
2019-07-25 15:20:44 -0700

    Created SearchBar to search for Chats to join.

--- Classes/ViewControllers/SearchViewController.swift
+++ Classes/ViewControllers/SearchViewController.swift
@@ -8,38 +8,79 @@
 
 import UIKit
 
-class SearchViewController: UITableViewController {
+class SearchViewController: UIViewController {
+  
+  @IBOutlet weak var searchBar: UISearchBar!
+  @IBOutlet weak var tableView: UITableView!
   
   private let reuseIdentifier = "SearchCellIdentifier"
   private var chats: [Chat] = []
+  private var filteredChats: [Chat] = []
   
   override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    setupData()
+    tableView.reloadData()
+  }
+  
+  @IBAction func viewTapped(_ sender: UITapGestureRecognizer) {
+    let location = sender.location(in: view)
+    if searchBar.isFirstResponder && !searchBar.frame.contains(location) {
+      searchBar.resignFirstResponder()
+    }
+  }
+  
+  private func setupData() {
     chats = MockLoader.loadChats()
+    filteredChats = chats
   }
   
-  override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-    return 44
-  }
+}
+
+extension SearchViewController: UITableViewDataSource {
   
-  override func numberOfSections(in tableView: UITableView) -> Int {
+  func numberOfSections(in tableView: UITableView) -> Int {
     return 1
   }
   
-  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return chats.count
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return filteredChats.count
   }
   
-  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    // Temporary until we get data
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
     
-    if let searchItemRowView = itemRowView(for: cell) as? SearchItemRowView {
-      let chat = chats[indexPath.row]
+    if let searchItemRowView = tableView.itemRowView(for: cell) as? SearchItemRowView {
+      let chat = filteredChats[indexPath.row]
       searchItemRowView.titleLabel.text = chat.name
       ImageLoader.load(chat.imageUrl) { searchItemRowView.imageView.image = $0 }
     }
     
     return cell
+  }
+  
+}
+
+extension SearchViewController: UITableViewDelegate {
+  
+  func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    return 44
+  }
+  
+  func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+    if searchBar.isFirstResponder {
+      searchBar.resignFirstResponder()
+    }
+  }
+  
+}
+
+extension SearchViewController: UISearchBarDelegate {
+  
+  func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+    filteredChats = searchText.isEmpty ? chats : chats.filter { $0.name.contains(searchText) }
+    tableView.reloadData()
   }
   
 }
