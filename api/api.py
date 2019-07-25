ca6fdfd02cae10fbc4d2f7554bbf6c099475c92b
Ngoni <ngoni.ndhlala@duke.edu>
2019-07-25 14:20:30 -0700

    creating realistic chat names

--- api/api.py
+++ api/api.py
@@ -4,6 +4,7 @@ connection = httplib.HTTPSConnection('parseapi.back4app.com', 443)
 connection.connect()
 
 def get_user_chats(user_id):
+  #Returns the chat_ids for the user's chats
   url = '/classes/Users?where={"user_id":' + str(user_id)+'}'
   connection.request('GET', url ,'', {
        "X-Parse-Application-Id": "OUS5D6T6Hf9C6yW6SJMOgWGIfaw7gZRbVHSMAfZi",
@@ -13,6 +14,7 @@ def get_user_chats(user_id):
   return result['results'][0]['chat_ids']
 
 def get_user(user_id):
+  #Returns the user object
   url = '/classes/Users?where={"user_id":' + str(user_id)+'}'
   connection.request('GET', url ,'', {
        "X-Parse-Application-Id": "OUS5D6T6Hf9C6yW6SJMOgWGIfaw7gZRbVHSMAfZi",
