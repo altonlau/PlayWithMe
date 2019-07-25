07f4a340c732d6a5da71fcc5f6650f6aeebe05dc
Ngoni <ngoni.ndhlala@duke.edu>
2019-07-25 14:14:50 -0700

    api

---
+++ api/api.py
@@ -0,0 +1,27 @@
+import json,httplib,urllib
+
+connection = httplib.HTTPSConnection('parseapi.back4app.com', 443)
+connection.connect()
+
+def get_user_chats(user_id):
+  url = '/classes/Users?where={"user_id":' + str(user_id)+'}'
+  connection.request('GET', url ,'', {
+       "X-Parse-Application-Id": "OUS5D6T6Hf9C6yW6SJMOgWGIfaw7gZRbVHSMAfZi",
+       "X-Parse-REST-API-Key": "uyWgbdC2Y0sXFxS3H7SQt8cn0GkKigwSgwTzCybM"
+     })
+  result = json.loads(connection.getresponse().read())
+  return result['results'][0]['chat_ids']
+
+def get_user(user_id):
+  url = '/classes/Users?where={"user_id":' + str(user_id)+'}'
+  connection.request('GET', url ,'', {
+       "X-Parse-Application-Id": "OUS5D6T6Hf9C6yW6SJMOgWGIfaw7gZRbVHSMAfZi",
+       "X-Parse-REST-API-Key": "uyWgbdC2Y0sXFxS3H7SQt8cn0GkKigwSgwTzCybM"
+     })
+  result = json.loads(connection.getresponse().read())
+  print result['results'][0]
+
+
+
+
+
