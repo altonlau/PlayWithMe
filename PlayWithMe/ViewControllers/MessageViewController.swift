97e8c10273d564b9ab16664046c26841aa574f34
Stephen Lee <stephel@yelp.com>
2019-07-25 14:09:29 -0700

    WIP message view controller

---
+++ PlayWithMe/ViewControllers/MessageViewController.swift
@@ -0,0 +1,104 @@
+//
+//  MessageViewController.swift
+//  PlayWithMe
+//
+//  Created by Stephen Lee on 7/25/19.
+//  Copyright © 2019 Yelp Inc. All rights reserved.
+//
+
+import UIKit
+import MessageKit
+
+var messages: [Message] = [Message(
+    member: Member(name: "bluemoon", color: .blue),
+    text: "text",
+    messageId: "123")]
+var member: Member!
+
+class MessageViewController: MessagesViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        member = Member(name: "bluemoon", color: .blue)
+        messagesCollectionView.messagesDataSource = self
+        messagesCollectionView.messagesLayoutDelegate = self
+        messageInputBar.delegate = self
+        messagesCollectionView.messagesDisplayDelegate = self
+    }
+}
+
+extension MessageViewController: MessagesDataSource {
+    func numberOfSections(
+        in messagesCollectionView: MessagesCollectionView) -> Int {
+        return messages.count
+    }
+    
+    func currentSender() -> SenderType {
+        return Sender(id: member.name, displayName: member.name)
+    }
+    
+    func messageForItem(
+        at indexPath: IndexPath,
+        in messagesCollectionView: MessagesCollectionView) -> MessageType {
+        
+        return messages[indexPath.section]
+    }
+    
+    func messageTopLabelHeight(
+        for message: MessageType,
+        at indexPath: IndexPath,
+        in messagesCollectionView: MessagesCollectionView) -> CGFloat {
+        
+        return 12
+    }
+    
+    func messageTopLabelAttributedText(
+        for message: MessageType,
+        at indexPath: IndexPath) -> NSAttributedString? {
+        
+        return NSAttributedString(
+            string: message.sender.displayName,
+            attributes: [.font: UIFont.systemFont(ofSize: 12)])
+    }
+}
+
+
+extension MessageViewController: MessagesLayoutDelegate {
+    func heightForLocation(message: MessageType,
+                           at indexPath: IndexPath,
+                           with maxWidth: CGFloat,
+                           in messagesCollectionView: MessagesCollectionView) -> CGFloat {
+        
+        return 0
+    }
+}
+
+extension MessageViewController: MessagesDisplayDelegate {
+    func configureAvatarView(
+        _ avatarView: AvatarView,
+        for message: MessageType,
+        at indexPath: IndexPath,
+        in messagesCollectionView: MessagesCollectionView) {
+        
+        _ = messages[indexPath.section]
+        let color = #colorLiteral(red: 0.1283891797, green: 0.6054974198, blue: 1, alpha: 1)
+        avatarView.backgroundColor = color
+    }
+}
+
+extension MessageViewController: MessageInputBarDelegate {
+    func messageInputBar(
+        _ inputBar: MessageInputBar,
+        didPressSendButtonWith text: String) {
+        
+        let newMessage = Message(
+            member: member,
+            text: text,
+            messageId: UUID().uuidString)
+        
+        messages.append(newMessage)
+        inputBar.inputTextView.text = ""
+        messagesCollectionView.reloadData()
+        messagesCollectionView.scrollToBottom(animated: true)
+    }
+}
